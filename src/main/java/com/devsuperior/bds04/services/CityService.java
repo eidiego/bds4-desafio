e8a078131506818be3c1f1a7994025ede2eba164
Diego Rodrigues Barbosa <diegokod@gmail.com>
2023-01-03 23:26:57 -0300

    All tests passing

---
+++ src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -0,0 +1,41 @@
+package com.devsuperior.bds04.services;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CityService {
+
+	@Autowired
+	private CityRepository repository;
+	
+	public List<CityDTO> findAll() {
+		List<City> list = repository.findAll(Sort.by("name"));
+		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public CityDTO insert(CityDTO dto) {
+		City entity = new City();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CityDTO(entity);
+	}
+
+}
